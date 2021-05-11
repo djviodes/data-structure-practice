ba663c96ce8ca594dda96931a03fc027ed5361de
djviodes <djviodes26@gmail.com>
2021-05-11 00:06:48 -0700

    Added an isBalanced function, as well as traversal functions for the Binary Search Tree.

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -137,6 +137,114 @@ class BST {
 
     this.root = removeNode(this.root, data);
   }
+
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+
+  findMinHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+
+  findMaxHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+
+  inOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      let result = new Array();
+      const traverseInOrder = (node) => {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      };
+
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+
+  preOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      let result = new Array();
+      const traversePreOrder = (node) => {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      };
+
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+
+  postOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      let result = new Array();
+      const traversePostOrder = (node) => {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      };
+
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  levelOrder() {
+    let result = [];
+    let Q = [];
+
+    if (this.root != null) {
+      Q.push(this.root);
+
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+
+        if (node.left != null) {
+          Q.push(node.left);
+        }
+
+        if (node.right != null) {
+          Q.push(node.right);
+        }
+      }
+
+      return result;
+    } else {
+      return null;
+    }
+  }
 }
 
 const bst = new BST();
@@ -151,8 +259,8 @@ bst.add(8);
 bst.add(1);
 bst.add(9);
 bst.add(10);
-bst.remove(4);
-console.log(bst.findMin());
-console.log(bst.findMax());
-console.log(bst.find(5));
-console.log(bst.isPresent(4));
+console.log(bst.isBalanced());
+console.log(bst.inOrder());
+console.log(bst.preOrder());
+console.log(bst.postOrder());
+console.log(bst.levelOrder());
